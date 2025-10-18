1d82b59d4444c0a1e9f32eeedc3e171f7ff890d0
Ruveena Gamage <ruveenasinali@gmail.com>
2025-10-18 12:06:37 +0530

    Update database.php

---
+++ database/database.php
@@ -0,0 +1,34 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = ""; 
+    private $dbname = "licensexpress";
+    private $conn;
+
+    public function __construct() {
+        $this->connect();
+    }
+
+    private function connect() {
+        
+        $this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->dbname);
+
+        if (!$this->conn) {
+            die("❌ Database Connection Failed: " . mysqli_connect_error());
+        } else {
+            
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+
+    public function closeConnection() {
+        if ($this->conn) {
+            mysqli_close($this->conn);
+        }
+    }
+}
+?>
